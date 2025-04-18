4102b585a981e2aa97839c2a498cbb9de15cb47e
MD. Akhtarujjaman Siddiquee <akhtarujjamansiddiquee@gmail.com>
2025-04-18 17:12:58 +0600

    Add new files and updates

--- utils.py
+++ utils.py
@@ -10,3 +10,10 @@ def recommend(temp, humidity, pressure, cloud):
     avg_temp = temp
     hi_input = pd.DataFrame([[avg_temp, humidity, pressure, cloud]], columns=features)
     is_heatwave = model.predict(hi_input)[0]
+      # Time Slot Suggestion
+    if avg_temp <= 33:
+        time_slots = ["6:00 AM - 10:00 AM", "5:30 PM - 8:00 PM"]
+    elif avg_temp <= 37:
+        time_slots = ["6:00 AM - 8:00 AM", "6:30 PM - 7:30 PM"]
+    else:
+        time_slots = ["⚠️ Stay Indoors - Too Hot Outside"]
